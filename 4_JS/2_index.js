a71459784376247c510418a9c15bea7ec7828865
Yathaarth Suri <yathaarthsuri@gmail.com>
2022-02-21 10:22:08 +0530

    Add files via upload

---
+++ 4_JS/2_index.js
@@ -0,0 +1,11 @@
+// Creator of JS : Brendon Eich in 10 days
+
+// Can use single or double inverted commas
+// Semi colon is not necessary at most places
+
+// Run in Browser using Live Server
+console.log('Hello from browser')
+
+
+// Ryan Dahl was the creator of NodeJS, before Ryan Dahl, JS could only be run on the web, but he proposed the idea of running JS on machine and this gave birth to frontend, backend, libraries and frameworks
+
---
+++ 4_JS/3_test.js
@@ -0,0 +1,5 @@
+// Running JS in console or machine
+console.log("Hello from Node")
+
+// To run above, write (node filename.js)
+
